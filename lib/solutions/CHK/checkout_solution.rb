9e94ea19ed633254d708512ef291d5ab22c4a26a
sbx_user1051 <sbx_user1051@169.254.39.143>
2025-06-13 12:31:16 +0000

    Fri Jun 13 12:31:16 UTC 2025

--- lib/solutions/CHK/checkout_solution.rb
+++ lib/solutions/CHK/checkout_solution.rb
@@ -2,8 +2,8 @@
 class CheckoutSolution
 
   def checkout(skus)
-    %w[A B C D].map { |sku| { sku => skus.upcase.count(sku) } }
-    item_count = { A: , B:, C:, D:}
+    item_count = %w[A B C D].map { |sku| { sku => skus.upcase.count(sku) } }
   end
 
 end
+
