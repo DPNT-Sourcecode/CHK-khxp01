fa5ffb9aabf3c1beb7e41b1c0a176c0972a247f6
sbx_user1051 <sbx_user1051@169.254.2.229>
2025-06-13 07:46:14 +0000

    Fri Jun 13 07:46:14 UTC 2025

--- lib/solutions/SUM/sum_solution.rb
+++ lib/solutions/SUM/sum_solution.rb
@@ -1,8 +1,12 @@
 # noinspection RubyUnusedLocalVariable
 class SumSolution
+  class SumError < StandardError; end
 
   def compute(x, y)
-    raise 'Not implemented'
-  end
+    raise SumError, "#{x} is not a postive integer between 0-100" if x < 0 || x > 100
+    raise SumError, "#{y} is not a postive integer between 0-100" if y < 0 || y > 100
 
+    x + y
+  end
 end
+
