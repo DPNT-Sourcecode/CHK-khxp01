6d0e49e216d98abab489f172a191b4574cf7aa82
sbx_user1051 <sbx_user1051@169.254.20.75>
2025-06-13 19:54:41 +0000

    Fri Jun 13 19:54:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.rb
+++ lib/solutions/CHK/checkout_solution.rb
@@ -27,17 +27,22 @@ class CheckoutSolution
     item_counts.reduce(0) do |sum, (sku, quantity)|
       price_table = PRICES[sku.to_sym]
 
-      sum += calculate_special_price(quantity, price_table) if %w[A B].include?(sku)
+      special_price_sum, offer_index = *calculate_special_price(quantity, price_table[:special_offers])
 
-      sum += regular_price_count * price_table[:price]
+      sum += special_price_sum if %w[A B].include?(sku)
+
+      sum += regular_price_count(sku, quantity, price_table[:special_offers][offer_index][:quantity]) * price_table[:price]
     end
   end
 
-  def calculate_special_price(quantity, price_table)
-    quantity / price_table[:special_quantity] * price_table[:special_price]
+  def calculate_special_price(quantity, special_offers)
+    results = special_offers.map { |offer| quantity / offer[:quantity] * offer[:price] }
+
+    { price: results.min, offer_index: results.index(results.min) }
   end
 
-  def regular_price_count(sku, quantity, price_table)
-    %w[A B].include?(sku) ? quantity % price_table[:special_quantity] : quantity
+  def regular_price_count(sku, quantity, special_quantity)
+    %w[A B].include?(sku) ? quantity % special_quantity : quantity
   end
 end
+
