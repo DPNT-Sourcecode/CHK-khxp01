4f97fc60b7e8bad1c54cc4d280cf6ac7aaf5e0f7
sbx_user1051 <sbx_user1051@169.254.39.143>
2025-06-13 12:16:18 +0000

    Fri Jun 13 12:16:18 UTC 2025

--- lib/solutions/HLO/hello_solution.rb
+++ lib/solutions/HLO/hello_solution.rb
@@ -1,7 +1,8 @@
 # noinspection RubyUnusedLocalVariable
 class HelloSolution
   def hello(friend_name)
-    'Hello, World!'
+    "Hello, #{friend_name}!"
   end
 end
+
 
--- spec/HLO/hello_spec.rb
+++ spec/HLO/hello_spec.rb
@@ -1,19 +1,10 @@
 require_solution 'HLO'
 
 describe HelloSolution do
-  let(:expected_string) { 'Hello, World!'}
-
-  context 'when friend_name is present' do
-    it "returns the expected string" do
-      expect(HelloSolution.new.hello('Nuno')).to eq expected_string
-    end
+  it "returns the expected string" do
+    expect(HelloSolution.new.hello('Nuno')).to eq "Hello, Nuno!"
   end
-
-  context 'when friend_name is present' do
-    it "returns the expected string" do
-      expect(HelloSolution.new.hello(nil)).to eq expected_string
-    end
-  end 
 end
+
 
 
