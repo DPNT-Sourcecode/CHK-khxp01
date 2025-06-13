ff6bb10bdd9a2f67eeaa8c5e1bad184a4f361d6f
sbx_user1051 <sbx_user1051@169.254.20.75>
2025-06-13 19:18:40 +0000

    Fri Jun 13 19:18:40 UTC 2025

--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -19,15 +19,6 @@ describe CheckoutSolution do
     end
   end
 
-  context 'with input AABACDDA' do
-    let(:skus) { 'AABADDA' } # A -> 1*130 + 1*50, B -> 1*30, C -> 0*20, D -> 2*15
-    let(:expected_sum) { 1*130 + 1*50 + 1*30 + 0*20 + 2*15 }
-
-    it 'returns the correct checkout sum' do
-      expect(service_call).to eq(expected_sum)
-    end
-  end
-
   context 'with input Aa' do
     let(:skus) { 'Aa' }
 
@@ -35,5 +26,15 @@ describe CheckoutSolution do
       expect(service_call).to eq(-1)
     end
   end
+
+  context 'with input AABACDDAA' do
+    let(:skus) { 'AABADDA' } # A -> 1*130 + 1*50, B -> 1*30, C -> 0*20, D -> 2*15
+    let(:expected_sum) { 1*130 + 1*50 + 1*30 + 0*20 + 2*15 }
+
+    it 'returns the correct checkout sum' do
+      expect(service_call).to eq(expected_sum)
+    end
+  end
 end
+
 
