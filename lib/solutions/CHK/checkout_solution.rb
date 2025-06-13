46365c460bb10f14443992cbfd975cd8e728f85b
sbx_user1051 <sbx_user1051@169.254.20.75>
2025-06-13 19:57:41 +0000

    Fri Jun 13 19:57:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.rb
+++ lib/solutions/CHK/checkout_solution.rb
@@ -29,6 +29,7 @@ class CheckoutSolution
 
       special_price_sum, offer_index = *calculate_special_price(quantity, price_table[:special_offers])
 
+      byebug
       sum += special_price_sum if %w[A B].include?(sku)
 
       sum += regular_price_count(sku, quantity, price_table[:special_offers][offer_index][:quantity]) * price_table[:price]
@@ -38,11 +39,12 @@ class CheckoutSolution
   def calculate_special_price(quantity, special_offers)
     results = special_offers.map { |offer| quantity / offer[:quantity] * offer[:price] }
 
-    { price: results.min, offer_index: results.index(results.min) }
+    [results.min, results.index(results.min)]
   end
 
   def regular_price_count(sku, quantity, special_quantity)
     %w[A B].include?(sku) ? quantity % special_quantity : quantity
   end
 end
+
 
