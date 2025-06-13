80f0ef38985b853804f1b588e51a95aa44e034ad
sbx_user1051 <sbx_user1051@169.254.39.143>
2025-06-13 11:49:26 +0000

    Fri Jun 13 11:49:26 UTC 2025

--- spec/SUM/sum_spec.rb
+++ spec/SUM/sum_spec.rb
@@ -2,12 +2,12 @@ require_solution 'SUM'
 
 context 'when inputs are in range' do
   it 'adds the numbers' do
-    expect(SumSolution.new.compute(1, 2).to eq(3))
+    expect(SumSolution.new.compute(1, 2)).to eq(3)
   end
 end
 
 context 'when an input is out of range' do
   it 'raises an error' do
-    expect { SumSolution.new.compute(-1, 2).to raise_error(SumSolution::SumError).with_message('-1 is not a postive integer between 0-100')}
+    expect { SumSolution.new.compute(-1, 2) }.to raise_error(SumSolution::SumError).with_message('-1 is not a postive integer between 0-100')
   end
 end
