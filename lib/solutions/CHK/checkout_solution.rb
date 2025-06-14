6da93ab1eb82bd4fde3a74c203f2d1177e2ad9cf
sbx_user1051 <sbx_user1051@169.254.95.127>
2025-06-14 14:57:08 +0000

    Sat Jun 14 14:57:08 UTC 2025

--- lib/solutions/CHK/checkout_solution.rb
+++ lib/solutions/CHK/checkout_solution.rb
@@ -2,21 +2,23 @@
 require 'byebug'
 class CheckoutSolution
 
+  ITEMS = %w[A B C D E].freeze
+
   GENERAL_PRICES = {
-    A: 50, B: 30, C: 20, D: 15, E: 40
+    'A' => 50, 'B' => 30, 'C' => 20, 'D' => 15, 'E' => 40
   }.freeze
 
   BULK_BUY_OFFERS = {
-    A: [{ quantity: 3, price: 130 }, { quantity: 5, price: 200 }],
-    B: [{ quantity: 2, price: 45 }]
+    'A' => [{ quantity: 3, price: 130 }, { quantity: 5, price: 200 }],
+    'B' => [{ quantity: 2, price: 45 }]
   }.freeze
 
   FREE_PRODUCT_OFFERS = {
-    E: { quantity: 2, sku: 'B' }
+    'E' => { quantity: 2, sku: 'B' }
   }.freeze
 
   def checkout(skus)
-    return -1 if skus.nil? || skus.class != String || skus.chars.any? { |ch| !%w[A B C D].include?(ch) }
+    return -1 if skus.nil? || skus.class != String || skus.chars.any? { |ch| !ITEMS.include?(ch) }
 
     item_counts = get_item_counts(skus)
 
@@ -33,23 +35,11 @@ class CheckoutSolution
   private
 
   def get_item_counts(skus)
-    Hash[%i[A B C D].map { |sku| [sku, skus.count(sku.to_s)] }]
+    Hash[ITEMS.map { |sku| [sku, skus.count(sku)] }]
   end
 
   def calculate_sum(item_counts)
     sum = 0
-
-    item_counts.reduce(0) do |sum, (sku, quantity)|
-      price_table = PRICES[sku.to_sym]
-      
-      if price_table[:special_offers].nil?
-        sum += quantity * price_table[:price]
-
-        next sum
-      end
-
-      sum += calculate_special_price(quantity, price_table)
-    end
 
     item_counts.each do |sku, quantity|
       BULK_BUY_OFFERS[sku].sort_by { |offer| -offer[:quantity] }.each do |offer|
@@ -59,7 +49,10 @@ class CheckoutSolution
 
       sum += quantity * GENERAL_PRICES[sku]
     end
+
+    sum
   end
 end
+
 
 
